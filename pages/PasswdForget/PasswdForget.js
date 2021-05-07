e3d3435c040e9a428ceb68a14ee1537ff11ca881
Leo-yang-1020 <506335091@qq.com>
2021-05-07 20:43:40 +0800

    登录页面

---
+++ pages/PasswdForget/PasswdForget.js
@@ -0,0 +1,29 @@
+// pages/PasswdForget/PasswdForget.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    option1: [
+      { text: '+86', value: 0 },
+      { text: '新款商品', value: 1 },
+      { text: '活动商品', value: 2 },
+    ],
+   
+    value1: 0,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
